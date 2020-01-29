a0a187a47601e40677d79bff6897f862b6f53144
aritchelle <aritchellecal@gmail.com>
2020-01-29 21:58:21 +0800

    Animated Side Bar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const openSlideMenu = () => {
+    document.getElementById('menu').style.width = '250px'
+    document.getElementById('content').style.marginLeft = '250px'
+}
+const closeSlideMenu = () => {
+    document.getElementById('menu').style.width = '0'
+    document.getElementById('content').style.marginLeft = '0'
+}
